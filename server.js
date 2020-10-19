0cdc725aaa7fda44b234d5d4296c2cc00fbc8e86
Tatyana <tatyana.ryabinina@technorely.com>
2020-10-19 18:03:11 +0300

    Emitting events

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-const server = require('http').createServer()
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-const io = require('socket.io')();
-
-server.listen(3000)
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
 
 io.on('connection', (socket) => {
-    socket.join('room 237', () => {
-        let rooms = Object.keys(socket.rooms);
-        console.log(rooms); // [ <socket.id>, 'room 237' ]
+    // socket.broadcast.emit('cnt', 'connected ' + socket.id );
+    socket.on('request', (msg) => {
+        const number = Number(msg);
+        if(!!number || number === 0) socket.emit('response', number + 1);
+        else socket.emit('response', 'from: ' + socket.id + msg);
     });
+    // socket.broadcast.emit('close', 'closed ' + socket.id);
 });
 
-io.on('connect', (socket) => {
-    console.log('111111111111111111111111');
-    socket.binary(false).emit('an event', { some: 'data' }); // The data to send has no binary data
-});
-io.sockets.emit('hi', 'everyone');
-
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+})
